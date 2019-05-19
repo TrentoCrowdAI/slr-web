661d91da77e7e14b3c270c56b152fa5614b9bcb6
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-05-19 18:55:05 +0200

    move routes to main component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ import {HashRouter as Router, Route, Switch} from "react-router-dom";
 
 
 import Main from 'components/main';
-import Home from 'components/home';
 
 import NavBar from 'components/navigation/navBar';
 import SideMenu from 'components/navigation/sideMenu';
 import BreadCrumbs from 'components/breadCrumbs';
 
 import UsersLoginLogout from 'components/usersLoginLogout';
-
-import ProjectsList from 'components/projects/projectsList';
-import ProjectPage from 'components/projects/projectPage';
 
 
 import  {AppProvider} from 'components/providers/appProvider';
@@ -39,18 +35,11 @@ const App = function(props) {
                         </NavBar>
                         <Switch>
                             {/* this route will always be rendered*/}
-                            <Route render={(props) => <><BreadCrumbs {...props}/><UsersLoginLogout {...props}/></>}/>
-                        </Switch>         
-                        <Main>
-                            <Switch>
-                                <Route exact path="/" render={() => <Home/> }/>
-                                <Route exact path="/projects" render={(props) => <ProjectsList {...props} />}/>
-                                <Route path="/projects/:id" render={(props) => <ProjectPage {...props} />}/>
-                                <Route render={(props) => <div>404</div>}/>
-                            </Switch>
+                            <Route render={(props) => <div className="relative-header"><BreadCrumbs {...props}/><UsersLoginLogout {...props}/></div>}/>
+                        </Switch> 
 
-                        </Main>
-
+                        <Main/>
+                    
                     </AppProvider>
                 </div>
             </Router>
--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,56 @@
-import React from "react";
+import React, {useContext} from "react";
+import {Route, Switch} from "react-router-dom";
+
+import {AppContext} from 'components/providers/appProvider';
+
+import Home from 'components/home';
+import ProjectsList from 'components/projects/projectsList';
+import ProjectPage from 'components/projects/projectPage';
+import LoadIcon from 'components/svg/loadIcon';
 
 /**
  * compotent main of page
  */
 const Main = function(props){
 
-        return (
-            <div className="main-wrapper">
-                {props.children}
-            </div>
+    let output = "";
+
+    //get data from global context
+    const appConsumer = useContext(AppContext);
+
+    console.log(typeof appConsumer.userFetch);
+
+    console.log(appConsumer.userFetch);
+
+    console.log(appConsumer.user);
+
+    //If I'm fecthing the user I dispay a loading icon
+    if(appConsumer.userFetch){
+        output = (<LoadIcon/>);
+    }
+    //I render routes only if the user is logged
+    else if(appConsumer.user){
+        output = (
+            <Switch>
+                <Route exact path="/" render={() => <Home/> }/>
+                <Route exact path="/projects" render={(props) => <ProjectsList {...props} />}/>
+                <Route path="/projects/:id" render={(props) => <ProjectPage {...props} />}/>
+                <Route render={(props) => <div>404</div>}/>
+            </Switch>
         );
+    }
+    //otherwise if the user is not logged in display the home
+    else if(!appConsumer.user){
+        output = (<Home/>);
+    }else{
+        output = (<LoadIcon/>);
+    }
+
+    return (
+        <div className="main-wrapper">
+            {output}
+        </div>
+    );
 };
 
 export default Main;
