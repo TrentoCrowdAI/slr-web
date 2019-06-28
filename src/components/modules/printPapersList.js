fc22dc56aab6dc8b444bdfbb3e87b9cb4853c252
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2019-06-28 16:05:58 +0200

    more rearrangement

--- src/components/modules/printPapersList.js
+++ src/components/modules/printPapersList.js
@@ -10,7 +10,7 @@ import {AppContext} from 'components/providers/appProvider'
 import NoPapers from "components/svg/noPapers";
 
 import {join} from 'utils/index';
-import PaperConfidence from "components/projects_page/paperConfidence";
+import PaperConfidence from "components/projects_page/search_tab/search_automated/paperConfidence";
 /**
  * prints the papers list of a local search on the fake database
  */
